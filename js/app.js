c00970284d9a323b5c913a9fa3ef95378e2b111d
CristianoSFMothe <cristianodevsystem@gmail.com>
2024-04-13 22:12:42 -0300

    :up:  feat: animações

--- js/app.js
+++ js/app.js
@@ -2,23 +2,22 @@
 var countDownDate = new Date("April 25, 2025 15:37:25").getTime();
 
 //Atualiza a contagem regressiva a cada 1 segundo
-var x = setInterval(function() {
-
-// Obtém a data e hora de hoje
+var x = setInterval(function () {
+  // Obtém a data e hora de hoje
   var now = new Date().getTime();
 
   // Encontre a distância entre agora e a data da contagem regressiva
   var distance = countDownDate - now;
 
-    // Time calculations for days, hours, minutes and seconds
+  // Time calculations for days, hours, minutes and seconds
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Exibe o resultado no elemento com id="demo"
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
+  document.getElementById("demo").innerHTML =
+    days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
 
   // Se a contagem regressiva terminar, escreva algum texto
   if (distance < 0) {
@@ -34,4 +33,33 @@ var swiper = new Swiper(".mySwiper", {
     el: ".swiper-pagination",
     clickable: true,
   },
+});
+
+// Animations
+ScrollReveal().reveal(".top_nav", {
+  origin: "bottom",
+  // distance: "20px",
+  opacity: 0,
+});
+
+ScrollReveal().reveal(".nav", {
+  origin: "bottom",
+  distance: "20px",
+  opacity: 0,
+  delay: 100,
+});
+
+ScrollReveal().reveal(".header", {
+  origin: "bottom",
+  distance: "20px",
+  opacity: 0,
+  delay: 200,
+});
+
+ScrollReveal().reveal(".section", {
+  origin: "bottom",
+  distance: "20px",
+  opacity: 0,
+  duration: 1000,
+  delay: 100,
 });
